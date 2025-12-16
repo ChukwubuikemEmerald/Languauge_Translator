eab3d5525aa8c180cc865549ac37aca469817ff3
Fash <godsfavourfasola@gmail.com>
2025-12-16 15:52:30 +0100

    yoruba translation by Fasola Godsfavour

--- LANGUAGE_TRANSLATOR.py
+++ LANGUAGE_TRANSLATOR.py
@@ -1,1 +1,30 @@
-print("Language Translator")
+#fasola omogbolahan
+yoruba_dictionary  = {
+    "hello": "Ẹ ǹlẹ́",
+    "bye" : "Ó dàbọ̀",
+    "good morning" : "e kaaro",
+    "good evening" : "ka a ale",
+    "welcome" : "kaabo",
+    "thank you" : "e dupe",
+    "food" : "Ounjẹ", 
+    "well done" : "daradara ṣe",
+    "how are you" : "Bawo ni",
+    "come" : "wá",
+    "go" : "lọ",
+    "father" : "baba",
+    "mother" : "iya",
+    "boy" : "ọmọkunrin",
+    "girl" : "omobirin",
+    "money" : "owo",
+    "what is your name" : "Kí ni orúkọ rẹ",
+    "brother" : "arakunrin",
+    "sister" : "arabinrin",
+    "school" : "ile-iwe",
+
+}
+
+igbo_dictionary = {
+
+}
+
+
